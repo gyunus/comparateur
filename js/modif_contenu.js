6d553b290295aa2bc27752248cb4058145ba5b8f
jyuza <bricetonneau@gmail.com>
2012-03-20 21:05:49 +0100

    Mise en ligne du template

---
+++ js/modif_contenu.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$('header h1').append(' test ajout');
+$('header h1').html('Votre nouveau titre'); //Modification du Titre de la Page
+$('#article1 h1').html('Votre nouveau titre pour article 1');
+$('aside h1').html('Votre nouveau titre pour asside');
+$('aside p').html('Votre nouveau contenu pour asside');
+$('#article1 p').html('Votre nouveau contenu pour Article 1');
